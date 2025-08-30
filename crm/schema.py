35149d1759975aec9e4e5eb5d367cbd9f000de53
Mrdnkosi2252 <133382721+Mrdnkosi2252@users.noreply.github.com>
2025-08-30 13:27:37 +0200

    Update schema.py

--- crm/schema.py
+++ crm/schema.py
@@ -1,3 +1,12 @@
+import graphene
+from graphene_django import DjangoObjectType
+from crm.models import Product  
+
+class ProductType(DjangoObjectType):
+    class Meta:
+        model = Product
+        fields = ("id", "name", "stock", "price")
+
 class UpdateLowStockProducts(graphene.Mutation):
     class Arguments:
         pass
@@ -6,21 +15,29 @@ class UpdateLowStockProducts(graphene.Mutation):
     message = graphene.String()
 
     def mutate(self, info):
-        from products.models import Product
         
         low_stock_products = Product.objects.filter(stock__lt=10)
-        updated_products = []
         
+        
+        updated_products = []
         for product in low_stock_products:
             product.stock += 10
             product.save()
             updated_products.append(product)
-            
+        
         return UpdateLowStockProducts(
             products=updated_products,
-            message=f"Updated {len(updated_products)} products"
+            message=f"Updated {len(updated_products)} products with low stock"
         )
 
 class Mutation(graphene.ObjectType):
     update_low_stock_products = UpdateLowStockProducts.Field()
+
+
+class Query(graphene.ObjectType):
+    hello = graphene.String(default_value="Hello!")
     
+    def resolve_hello(self, info):
+        return "Hello from GraphQL CRM!"
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
