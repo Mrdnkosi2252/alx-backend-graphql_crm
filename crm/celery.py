6d03c92c15b733f3d3a5e1cfb50a4c1b2ef9e9df
Mrdnkosi2252 <133382721+Mrdnkosi2252@users.noreply.github.com>
2025-08-30 14:37:19 +0200

    Update celery.py

--- crm/celery.py
+++ crm/celery.py
@@ -1,1 +1,19 @@
 
+from celery import Celery
+import os
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crm.settings')
+
+app = Celery('crm')
+
+
+
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+
+app.autodiscover_tasks()
+
+@app.task(bind=True, ignore_result=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}')
